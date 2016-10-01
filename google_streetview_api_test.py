3257fbc06e3ca78736e134309e572f9f7af747e6
alliewarren <warrena@carleton.edu>
2016-10-01 17:17:51 -0500

    getting GSV images with varying headng and pitch

--- google_streetview_api_test.py
+++ google_streetview_api_test.py
@@ -5,9 +5,13 @@ import os
 
 def get_carleton_streetview():
     apiKey = 'AIzaSyBJN20CtR9R9AdWsInx-sAF_CoLRkI66so'
-    urlstring = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=1+North+College+Street,+Northfield,+MN+55057&heading=151.78&pitch=-0.76&key=' + apiKey
-    saveLoc = "C:/Users/evierosenberg/Desktop"
-    fileName = "CarletonImage.jpg"
-    urlretrieve(urlstring, fileName)
+    #gets google street view images with varying heading (horizontal view) and pitch (vertical view)
+    for i in range(0,7):
+        for j in range(0,7):
+            heading = str(i*50)
+            pitch = str(j*10)
+            urlstring = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=1+North+College+Street,+Northfield,+MN+55057&heading=' + heading + '&pitch=' + pitch + '&key=' + apiKey
+            fileName = "CarletonImage" + heading + "," + pitch + ".jpg"
+            urlretrieve(urlstring, fileName)
     
 get_carleton_streetview()
