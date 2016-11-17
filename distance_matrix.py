32eb1263299a7830611d9ef18a4ab9ab59b5ec66
alliewarren <warrena@carleton.edu>
2016-11-17 12:58:49 -0600

    improved efficiency of creating in distance graph

--- distance_matrix.py
+++ distance_matrix.py
@@ -6,7 +6,7 @@ import os
 from geopy.distance import vincenty
 
 
-def get_distances(coordinates, final, matrix):
+def get_distances_slow(coordinates, final, matrix):
     apiKey = 'AIzaSyBpaOfrcYIpU-7jb-M4zOAyHgBpzoPEoqg'
     for i in range(0, len(coordinates)):
         for j in range(0, len(coordinates)):
@@ -23,11 +23,38 @@ def get_distances(coordinates, final, matrix):
                     matrix[i][j] = result['rows'][0].get("elements")[0].get('duration').get('value')
     return matrix
 
+def get_distances(coordinates, final, matrix):
+    apiKey = 'AIzaSyBpaOfrcYIpU-7jb-M4zOAyHgBpzoPEoqg'
+    for i in range(0, len(coordinates)):
+        destinations = ""
+        destination_list = []
+        for j in range(0, len(coordinates)):
+            if i!=j:
+                start = str(coordinates[i][0]) + ',' + str(coordinates[i][1])
+                end = str(coordinates[j][0]) + ',' + str(coordinates[j][1])
+                distance1 = vincenty(coordinates[i], final).miles
+                distance2 = vincenty(coordinates[j], final).miles
+                if distance2>distance1:
+                    matrix[i][j] = 0
+                else:
+                    destinations = destinations + end + "|"
+                    destination_list.append(j)
+        destinations = destinations[:-1]
+        #REMEMBER limited to destination list of less than 100 (i think)
+        if len(destinations)>0:
+            urlstring = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + start + '&destinations=' + destinations + '&mode=driving&key=' + apiKey
+            result = simplejson.load(urlopen(urlstring))
+            for k in range(0, len(result['rows'][0].get("elements"))):
+                print(result['rows'][0].get("elements")[k].get('duration').get('value'))
+                matrix[i][destination_list[k]] = result['rows'][0].get("elements")[k].get('duration').get('value')
+    return matrix
+
 if __name__ == '__main__':
-    start = '49.7016339,-123.1558121'
-    end = '36.1699412,-115.1398296'
-    #seattle, sanFran, spokane, portland, bend
-    coordinates = [[47.6062095,-122.3320708], [37.7749295,-122.4194155], [47.6587802, -117.4260466], [45.5230622, -122.6764816], [44.0581728,-121.3153096]]
+    start = '49.7016339,-123.1558121' #vancouver
+    end = '36.1699412,-115.1398296' #las vegas
+    # vancouver, seattle, sanFran, spokane, portland, bend, las vegas
+    coordinates = [[49.7016339,-123.1558121], [47.6062095,-122.3320708], [37.7749295,-122.4194155], [47.6587802, -117.4260466], [45.5230622, -122.6764816], [44.0581728,-121.3153096], [36.1699412,-115.1398296]]
     w, h = len(coordinates), len(coordinates) 
-    matrix = [[0 for x in range(w)] for y in range(h)] 
+    matrix = [[0 for x in range(w)] for y in range(h)]
+
     print(get_distances(coordinates, end, matrix))
