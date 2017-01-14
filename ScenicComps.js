de82c87e760293ca2713b8fc539ae8d7f61707b9
Evie Rosenberg <rosenberge@carleton.edu>
2017-01-14 15:31:45 -0600

    Updated UI

--- ScenicComps.js
+++ ScenicComps.js
@@ -2,4 +2,23 @@ function initAutocomplete()
 {
     var autocomplete = new google.maps.places.Autocomplete( (document.getElementById('start_input')),
         {types: ['geocode']});
+    
+    var autocomplete = new google.maps.places.Autocomplete( (document.getElementById('end_input')),
+        {types: ['geocode']});
+}
+
+function getInputs()
+{
+    var geocoded_start = new XMLHttpRequest();          
+    geocoded_start.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCPn6VnGvKDcPjjnq9utmKMwVxPv0gln5w", false);
+    geocoded_start.send(); 
+    
+    var startCoords = [];
+    startCoords.push(geocoded_start["results"["geometry"["location"["lat"]]]]);    
+    startCoords.push(geocoded_start["results"["geometry"["location"["lng"]]]]);
+    
+    
+//    var endCoords = ;
+//    var maxTime = ;
+//    var scenery = ;
 }
